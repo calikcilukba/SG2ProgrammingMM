e1ccd14382f2becc89ba10bda1f61066ea8468da
impalempalsapi <chalikfroza@gmail.com>
2019-11-06 20:01:04 +0700

    first commit

---
+++ SG2.m
@@ -0,0 +1,46 @@
+% angka1 = input('masukkan angka   :');
+% angka2 = input('masukkan angka 2 :');
+% op = input('masukkan operator:', 's');
+% switch op
+%     case {'+'}
+%         hasil = angka1 + angka2;
+%     case {'-'}
+%         hasil = angka1 - angka2;
+%     case {'*'}
+%         if(angka1 ~= 0 && angka2 ~= 0)
+%             hasil = angka1 * angka2;
+%         else
+%             hasil = 'terdapat angka 0';
+%         end
+%     case {'/'}
+%         if(angka1 ~= 0 && angka2 ~= 0)
+%             hasil = angka1 / angka2;
+%         else
+%             hasil = 'terdapat angka 0';
+%         end
+%     otherwise
+%         disp('operator tidak ada');
+% end
+% disp(hasil);
+% 
+% for i=1:1:20
+%     disp(i)
+% end
+
+% x = 1
+% while x < 10
+%     disp(x);
+%     x = x+1;
+%     if(x == 5)
+%         break;
+%     end
+% end
+
+% for i=1:5
+%     disp(i);
+%     for j=6:10
+%         disp(j);
+%     end
+%     break;
+% end
+
